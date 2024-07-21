14715a4030e3e7dd20b0de0915ff52ae356fa2ec
Paulo Miguel <paulomigueldasilva@hotmail.com>
2024-07-21 16:04:00 -0300

    primeiro commit

---
+++ src/main/java/com/example/estoquejava/models/Fornecedor.java
@@ -0,0 +1,132 @@
+package com.example.estoquejava.models;
+
+public class Fornecedor {
+    private String nome;
+    private int id;
+    private String cnpj;
+    private String telefone;
+    private String cep;
+    private int num;
+    private String rua;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+    public Fornecedor(String nome, int id, String cnpj, String telefone, String cep,
+                      int num, String rua, String complemento, String bairro, String cidade,
+                      String estado) {
+        this.nome = nome;
+        this.id = id;
+        this.cnpj = cnpj;
+        this.telefone = telefone;
+        this.cep = cep;
+        this.num = num;
+        this.rua = rua;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public Fornecedor() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Fornecedor{" +
+                "nome=" + nome +
+                ", cnpj=" + cnpj +
+                ", telefone=" + telefone +
+                '}';
+    }
+}
