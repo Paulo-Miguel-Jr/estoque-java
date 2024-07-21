c625e26144bebf8480fc98a9f13ea1828af9cdcc
Samara Accioly <samara_accioly@hotmail.com>
2024-07-21 17:15:56 -0300

    Create PedidoFornecedor.java

---
+++ src/main/java/com/example/estoquejava/models/PedidoFornecedor.java
@@ -0,0 +1,51 @@
+package com.example.estoquejava.models;
+import java.time.LocalDate;
+
+public class PedidoFornecedor {
+    private int numero;
+    private LocalDate dataPedido;
+    private Fornecedor fornecedor;
+
+    public PedidoFornecedor(int numero, LocalDate dataPedido, Fornecedor fornecedor) {
+        this.numero = numero;
+        this.dataPedido = dataPedido;
+        this.fornecedor = fornecedor;
+    }
+
+    public PedidoFornecedor() {
+
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public LocalDate getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(LocalDate dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    @Override
+    public String toString() {
+        return "PedidoFornecedor{" +
+                "numero=" + numero +
+                ", dataPedido=" + dataPedido +
+                ", fornecedor=" + fornecedor +
+                '}';
+    }
+}
