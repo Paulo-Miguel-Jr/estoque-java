dbc6b7b6e628f11aee1d03442044675bad5ee8b8
wendellvsilva <wendellcrtss@gmail.com>
2024-08-08 16:40:21 -0300

    bug javafx resolvido

--- src/main/java/com/example/estoquejava/helloJavaFX/HelloApplication.java
+++ src/main/java/com/example/estoquejava/helloJavaFX/HelloApplication.java
@@ -6,11 +6,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        URL resource = HelloApplication.class.getResource("hello-view.fxml");
+        if (resource == null) {
+            System.err.println("Erro: arquivo teste.fxml não encontrado!");
+            System.out.println(HelloApplication.class.getResource(""));
+            return;
+        }
+        System.out.println(resource);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(resource);
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/com/example/estoquejava/models/exceptions/FornecedorException.java
@@ -0,0 +1,5 @@
+package com.example.estoquejava.models.exceptions;
+
+public class FornecedorException extends RuntimeException {
+
+}
