2c7137e90e1d7a26a9237be676740ea63a6e9afc
Samara Accioly <samara_accioly@hotmail.com>
2024-08-11 20:32:54 -0300

    Create PedidoRepCheioException.java

---
+++ src/main/java/com/example/estoquejava/models/exceptions/PedidoRepCheioException.java
@@ -0,0 +1,7 @@
+package com.example.estoquejava.models.exceptions;
+
+public class PedidoRepCheioException extends RuntimeException {
+    public PedidoRepCheioException(String message) {
+        super(message);
+    }
+}
