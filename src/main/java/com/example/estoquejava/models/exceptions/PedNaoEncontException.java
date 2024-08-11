07e6791246b93e8ab802755b10d124a47a3c4e54
Samara Accioly <samara_accioly@hotmail.com>
2024-08-11 20:33:03 -0300

    Create PedNaoEncontException.java

---
+++ src/main/java/com/example/estoquejava/models/exceptions/PedNaoEncontException.java
@@ -0,0 +1,7 @@
+package com.example.estoquejava.models.exceptions;
+
+public class PedNaoEncontException extends RuntimeException {
+    public PedNaoEncontException (String message) {
+        super (message);
+    }
+}
