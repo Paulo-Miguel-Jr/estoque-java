1aca44cde0b3933323064ab0851008b6fd97236e
Samara Accioly <samara_accioly@hotmail.com>
2024-08-31 17:19:06 -0300

    Update Fachada.java

--- src/main/java/com/example/estoquejava/models/Fachada.java
+++ src/main/java/com/example/estoquejava/models/Fachada.java
@@ -5,6 +5,7 @@ public class Fachada {
     private static Fachada instacia;
 
     private UsuarioCadastro usuarioCadastro;
+    private PedidoCadastro pedidoCadastro;
 
 
 
@@ -19,5 +20,35 @@ public class Fachada {
     public void cadastrarUsuario(Usuario usuario){
         usuarioCadastro.cadastrarUsuario(usuario);
     }
+
+
+    public void criarPedido(Pedido pedido) {
+        pedidoCadastro.criarPedido(pedido); //perguntar a Paulo pq não está indo adicionarPedido
+    }
+
+    public void removerPedido(int numero)  {
+        pedidoCadastro.removerPedido(numero);
+    }
+
+    public void atualizarPedido(Pedido pedido)  {
+        pedidoCadastro.atualizarPedido(pedido);
+    }
+
+    public Pedido procurarPedido(int numero)  {
+        pedidoCadastro.procurarPedido(numero);
+    }
+
+    public void processarVenda(int numero) {
+        pedidoCadastro.processarVenda(numero);
+    }
+
+    public void listarPedidos() {
+        pedidoCadastro.listarPedido();
+    }
+
+    public void adicionarItemAoPedido(int numero, ItemPedido item) {
+        pedidoCadastro.adicionarItemPedido(item);
+    }
+
 
 }
