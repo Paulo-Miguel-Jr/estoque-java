43b8d0f3569d4def1fd3b2c1d721075bae1a1b48
Samara Accioly <samara_accioly@hotmail.com>
2024-08-31 15:40:24 -0300

    Update PedidoCadastro.java

--- src/main/java/com/example/estoquejava/models/PedidoCadastro.java
+++ src/main/java/com/example/estoquejava/models/PedidoCadastro.java
@@ -1,5 +1,7 @@
 package com.example.estoquejava.models;
 
+import com.example.estoquejava.models.exceptions.PedNaoEncontException;
+import com.example.estoquejava.models.exceptions.PedidoRepCheioException;
 import com.example.estoquejava.repository.PedidoRepositorio;
 
 public class PedidoCadastro {
@@ -9,4 +11,29 @@ public class PedidoCadastro {
         this.pedidoRepositorio = PedidoRepositorio.getInstance();
     }
 
+    public void criarPedido(Pedido pedido) throws PedidoRepCheioException {
+        pedidoRepositorio.adicionarPedido(pedido);
+    }
+
+    public void removerPedido(int numero) throws PedNaoEncontException {
+        pedidoRepositorio.removerPedido(numero);
+    }
+
+    public void atualizarPedido(Pedido pedido) throws PedNaoEncontException {
+        pedidoRepositorio.atualizarPedido(pedido);
+    }
+
+    public void procurarPedido(int numero) throws PedNaoEncontException {
+        pedidoRepositorio.procurarPedido(numero);
+    }
+
+    public void processarVenda(int numero) throws PedNaoEncontException {
+        pedidoRepositorio.processarVenda(numero);
+    }
+
+    public void listarPedido() {
+        pedidoRepositorio.listarPedidos();
+    }
+
+    //public void adicionarItemAoPedido (int idPedido, ItemPedido item) throws PedNaoEncontException {}
 }
