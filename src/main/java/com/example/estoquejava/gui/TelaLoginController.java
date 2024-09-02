281378d816ae53bc22db72e511f39110f186f249
Paulo Miguel <paulomigueldasilva@hotmail.com>
2024-09-02 14:30:41 -0300

    tela login basica corrigida

--- src/main/java/com/example/estoquejava/gui/TelaLoginController.java
+++ src/main/java/com/example/estoquejava/gui/TelaLoginController.java
@@ -1,29 +1,19 @@
 package com.example.estoquejava.gui;
 
 import com.example.estoquejava.ScreenManager;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TelaLoginController implements Initializable {
 
-    private ObservableList<String> listaDeOpcoes = FXCollections.observableArrayList();
 
     public TelaLoginController() {
-        listaDeOpcoes.add("Opção A");
-        listaDeOpcoes.add("Opção B");
-        listaDeOpcoes.add("Opção C");
+
     }
     @FXML
     private Button ok;
