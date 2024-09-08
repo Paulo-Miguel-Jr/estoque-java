cf859dda350654d6768a17c1b908072d6941a802
Samara Accioly <samara_accioly@hotmail.com>
2024-09-08 16:56:19 -0300

    Update IPedidoRepositorio.java

--- src/main/java/com/example/estoquejava/repository/interfaces/IPedidoRepositorio.java
+++ src/main/java/com/example/estoquejava/repository/interfaces/IPedidoRepositorio.java
@@ -8,13 +8,10 @@ import com.example.estoquejava.models.exceptions.PedidoRepCheioException;
 
 public interface IPedidoRepositorio {
 
-<<<<<<< Updated upstream
-
     void adicionarPedido(Pedido pedido) throws PedidoRepCheioException;//trocar por cadastrar
-=======
+
     //trocar numero por idPedido
-    void adicionarPedido(Pedido pedido) throws PedidoRepCheioException;
->>>>>>> Stashed changes
+    //void adicionarPedido(Pedido pedido) throws PedidoRepCheioException;
 
     void removerPedido(int numero) throws PedNaoEncontException;
 
@@ -24,14 +21,12 @@ public interface IPedidoRepositorio {
 
     Pedido procurarPedido(int numero) throws PedNaoEncontException;
 
-<<<<<<< Updated upstream
     void adicionarItemAoPedido (int numero, ItemPedido item) throws PedNaoEncontException;
-=======
+
     void processarVenda(int numero) throws PedNaoEncontException;
 
     //void processarVenda(int numero) throws PedNaoEncontException;
 
     //adicionarItemAoPedido (int idPedido, ItemPedido item)
->>>>>>> Stashed changes
 
 }
