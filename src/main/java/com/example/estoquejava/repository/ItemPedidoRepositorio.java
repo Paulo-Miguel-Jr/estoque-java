24a1229769acec0780deb8eaa3b4e1669664f248
Samara Accioly <samara_accioly@hotmail.com>
2024-09-10 11:25:53 -0300

    Update ItemPedidoRepositorio.java

--- src/main/java/com/example/estoquejava/repository/ItemPedidoRepositorio.java
+++ src/main/java/com/example/estoquejava/repository/ItemPedidoRepositorio.java
@@ -1,19 +1,22 @@
 package com.example.estoquejava.repository;
 
 import com.example.estoquejava.models.ItemPedido;
+import com.example.estoquejava.models.Pedido;
+import com.example.estoquejava.models.exceptions.ItemPedNaoEncontException;
+import com.example.estoquejava.models.exceptions.ItemPedidoRepCheioException;
+import com.example.estoquejava.models.exceptions.PedNaoEncontException;
+import com.example.estoquejava.models.exceptions.PedidoRepCheioException;
 import com.example.estoquejava.repository.interfaces.IItemPedidoRepositorio;
 
 public  class ItemPedidoRepositorio implements IItemPedidoRepositorio {
 
-    private ItemPedido[] Itens;
-    private int tamanho;
-    private int contador;
+    private ItemPedido[] itemPedido;
+    private int proximo;
     private static ItemPedidoRepositorio singletonPedRep;
 
     public ItemPedidoRepositorio (){
-        this.Itens = new ItemPedido[10];
-        this.tamanho = 10;
-        this.contador = 0;
+        this.itemPedido = new ItemPedido[100];
+        this.proximo = 0;
     }
 
     public static ItemPedidoRepositorio getInstance(){
@@ -23,136 +26,153 @@ public  class ItemPedidoRepositorio implements IItemPedidoRepositorio {
         return singletonPedRep;
     }
 
-
-    private void AumentarLista(){
-        ItemPedido[] aux = new ItemPedido[this.tamanho+10];
-
-        for(int i = 0; i < tamanho; i++) {
-           aux[i] = this.Itens[i];
-        }
-
-        this.Itens = aux;
-        this.tamanho += 10;
-    }
-
-
-    private void DiminuirLista(){
-        ItemPedido[] aux = new ItemPedido[tamanho-10];
-
-        for(int i = 0; i < this.tamanho; i++){
-            aux[i] = this.Itens[i];
-        }
-
-        this.Itens = aux;
-        this.tamanho -= 10;
-    }
-
-
-    public void InserirItemPedido(ItemPedido item){
-        if(!(contador<tamanho)){
-            AumentarLista();
-        }
-
-        contador++;
-        Itens[contador] = item;
-
-    }
-
-
-    public void RemoverItemPedido(int id, int quantidade){
-
-        for(int i = 0; i <= contador; i++){
-            if(this.Itens[i].getIdProduto() == id && this.Itens[i].getQuantidade() == quantidade){
-                this.Itens[i] = this.Itens[contador];
-                contador--;
-                i = contador+1;
-            }
-
-            if(i == contador){
-                throw new IllegalArgumentException("Item não existe na lista");
+    public int getIdItemPedido(int idItem) {
+        for (int i = 0; i < proximo; i++) {
+            if (itemPedido[i].getIdItem() == idItem) {
+                return i;
             }
         }
-
-        if (tamanho - contador >= 20){
-            DiminuirLista();
-        }
-
+        return -1;
     }
 
+    public void adicionarItem(ItemPedido novoItemPedido) throws ItemPedidoRepCheioException {
+        if (proximo < this.itemPedido.length) {
+            this.itemPedido[proximo] = novoItemPedido;
+            proximo++;
+        } else {
+            throw new ItemPedidoRepCheioException("Repositório cheio. Não é possível adicionar mais itens de pedido.");
+        }
+    }
 
-    public ItemPedido BuscarItemPedido(int id, int quantidade){
+    public void removerItemPedido(int idItem) throws ItemPedNaoEncontException {
+        int indice = getIdItemPedido(idItem);
+        if (indice == -1) {
+            throw new PedNaoEncontException("Pedido não encontrado.");
+        } else {
+            // Move todos os elementos após o índice para uma posição anterior
+            for (int i = indice; i < proximo - 1; i++) {
+                itemPedido[i] = itemPedido[i + 1];
+            }
+            // Limpa o último elemento, que foi movido para frente
+            itemPedido[proximo - 1] = null;
+            proximo--;
+        }
+    }
 
-        VerificarItemPedido(id);
+    public ItemPedido buscarItemPedido(int idItem) throws ItemPedNaoEncontException {
+        int indice = getIdItemPedido(idItem);
+        if (indice == -1) {
+            throw new PedNaoEncontException("Pedido não encontrado.");
+        } else {
+            return itemPedido[indice];
+        }
+    }
 
-        for(int i = 0; i <= contador; i++){
-            if(this.Itens[i].getIdProduto() == id
-            && this.Itens[i].getQuantidade() == quantidade){
-                return this.Itens[i];
+    public void listarItensPedidos() {
+        for (int i = 0; i < proximo; i++) {
+            if (itemPedido[i] != null) {
+                System.out.println(itemPedido[i]);
             }
         }
-        throw new IllegalArgumentException("Este Item não existe na lista.");
     }
 
-    public int VerificarItemPedido(int id){
-        for(int i = 0; i <= contador; i++){
-            if(this.Itens[i].getIdProduto() == id){
-                return 1;
-            }
-        }
 
-        throw new IllegalArgumentException("Este Item não existe na lista");
-    }
+  //  public void RemoverItemPedido(int id, int quantidade){
 
-    public void ModificarItemPedido(int id, int quantidade, int modificacao){
+  //      for(int i = 0; i <= contador; i++){
+   //         if(this.Itens[i].getIdProduto() == id && this.Itens[i].getQuantidade() == quantidade){
+  //              this.Itens[i] = this.Itens[contador];
+    //            contador--;
+      //          i = contador+1;
+        //    }
+//
+  //          if(i == contador){
+    //            throw new IllegalArgumentException("Item não existe na lista");
+      //      }
+        //}
+//
+  //      if (tamanho - contador >= 20){
+    //        DiminuirLista();
+      //  }
 
-        VerificarItemPedido(id);
 
-        for(int a = 0; a <= contador; a++){
-            if(this.Itens[a].getIdProduto() == id
-                    && this.Itens[a].getQuantidade() == quantidade){
-                this.Itens[a].setQuantidade(modificacao);
-            }
-        }
 
-    }
 
-    public int getQuantItensPedidos(int id){
+  //  public ItemPedido BuscarItemPedido(int id, int quantidade){
 
-        VerificarItemPedido(id);
+    //    VerificarItemPedido(id);
 
-        int aux = 0;
+   //     for(int i = 0; i <= contador; i++){
+   //         if(this.Itens[i].getIdProduto() == id
+   //         && this.Itens[i].getQuantidade() == quantidade){
+    //            return this.Itens[i];
+    //        }
+   //     }
+    //    throw new IllegalArgumentException("Este Item não existe na lista.");
+  //  }
 
-        for(int i = 0; i <= contador; i++){
-            if(this.Itens[i].getIdProduto() == id){
-                aux++;
-            }
-        }
+  //  public int VerificarItemPedido(int id){
+  //      for(int i = 0; i <= contador; i++){
+  //          if(this.Itens[i].getIdProduto() == id){
+  //              return 1;
+ //           }
+//        }
 
-        return aux;
-    }
+ //       throw new IllegalArgumentException("Este Item não existe na lista");
+ //   }
 
-    public int getQuantProdutosVendidos(int id){
+    //public void ModificarItemPedido(int id, int quantidade, int modificacao){
 
-        VerificarItemPedido(id);
+   //     VerificarItemPedido(id);
 
-        int aux = 0;
-        for(int i = 0; i <= contador; i++){
-            if(this.Itens[i].getIdProduto() == id){
-                aux += this.Itens[i].getQuantidade();
-            }
-        }
+   //     for(int a = 0; a <= contador; a++){
+  //          if(this.Itens[a].getIdProduto() == id
+  //                  && this.Itens[a].getQuantidade() == quantidade){
+  //              this.Itens[a].setQuantidade(modificacao);
+ //           }
+   //     }
 
-        return aux;
-    }
+    //}
 
-    public ItemPedido[] ListarItensPedidos(){
-        ItemPedido[] aux = new ItemPedido[contador+1];
+  //  public int getQuantItensPedidos(int id){
 
-        for(int i = 0; i <= contador; i++){
-            aux[i] = Itens[i];
-        }
+  //      VerificarItemPedido(id);
 
-        return aux;
-    }
+  //      int aux = 0;
+
+  //      for(int i = 0; i <= contador; i++){
+  //          if(this.Itens[i].getIdProduto() == id){
+  //              aux++;
+  //          }
+  //      }
+
+  //      return aux;
+  //  }
+
+ //   public int getQuantProdutosVendidos(int id){
+
+ //       VerificarItemPedido(id);
+
+      //  int aux = 0;
+  //      for(int i = 0; i <= contador; i++){
+        //    if(this.Itens[i].getIdProduto() == id){
+       //         aux += this.Itens[i].getQuantidade();
+        //    }
+ //       }
+
+ //       return aux;
+ //   }
+
+
+
+  //  public ItemPedido[] ListarItensPedidos(){
+  //      ItemPedido[] aux = new ItemPedido[contador+1];
+
+  //      for(int i = 0; i <= contador; i++){
+   //         aux[i] = Itens[i];
+ //       }
+
+  //      return aux;
+  //  }
 
 }
